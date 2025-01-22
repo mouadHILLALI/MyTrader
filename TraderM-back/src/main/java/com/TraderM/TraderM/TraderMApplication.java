621b5ee7ead91f3026a2239aebe7662f232d8b24
mouadHILLALI <mauadtoto001@gmail.com>
2025-01-22 02:03:35 +0100

    init the project

---
+++ TraderM-back/src/main/java/com/TraderM/TraderM/TraderMApplication.java
@@ -0,0 +1,13 @@
+package com.TraderM.TraderM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderMApplication.class, args);
+	}
+
+}
---
+++ TraderM-back/src/test/java/com/TraderM/TraderM/TraderMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TraderM.TraderM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
