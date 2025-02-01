71d19d5ee6b70413be3c9835410e193eca1ced38
mouadHILLALI <mauadtoto001@gmail.com>
2025-02-01 18:51:29 +0100

    TRDRM-10 adding an authfilter for jwt auth

---
+++ TraderM-back/src/main/java/com/TraderM/TraderM/config/JwtAuthFilter.java
@@ -0,0 +1,23 @@
+package com.TraderM.TraderM.config;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final String secretKey;
+
+    
+    public void doFilterInternal(HttpServletRequest req, 
+    HttpServletResponse res,
+     FilterChain filter) throws ServletException,IOException
+    {
+        
+    }
+}
--- TraderM-back/src/main/java/com/TraderM/TraderM/config/SecurityConfig.java
+++ TraderM-back/src/main/java/com/TraderM/TraderM/config/SecurityConfig.java
@@ -1,10 +1,23 @@
 package com.TraderM.TraderM.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       return http.csrf(crsf -> crsf.disable()).authorizeHttpRequests(
+        auth -> auth.requestMatchers("/api/public/**").permitAll()
+        .requestMatchers("/api/users/**").hasAnyAuthority("ROLE_USER")
+        .anyRequest().authenticated()
+       ).addFilterBefore(new JwtAuthFilter(), AuthorizationFilter.class).build();
+    }
 }
