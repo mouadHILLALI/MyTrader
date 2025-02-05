7d181de7775cbb4280999526aaa8c23a73d62f90
mouadHILLALI <mauadtoto001@gmail.com>
2025-02-05 10:58:50 +0100

    TRDRM-10 working on the auth

--- TraderM-back/src/main/java/com/TraderM/TraderM/config/JwtAuthFilter.java
+++ TraderM-back/src/main/java/com/TraderM/TraderM/config/JwtAuthFilter.java
@@ -13,7 +13,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final String secretKey;
 
-    
+    @Override
     public void doFilterInternal(HttpServletRequest req, 
     HttpServletResponse res,
      FilterChain filter) throws ServletException,IOException
--- TraderM-back/src/main/java/com/TraderM/TraderM/config/UserDetailsImpl.java
+++ TraderM-back/src/main/java/com/TraderM/TraderM/config/UserDetailsImpl.java
@@ -11,3 +11,4 @@ public class UserDetailsImpl implements UserDetailsService {
     
 }
 
+    
